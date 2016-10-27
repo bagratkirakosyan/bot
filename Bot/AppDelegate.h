4a1af81a9ebb4afa6db0500967630f09f31decf9
Bagrat Kirakosian <bagratkirakosian@gmail.com>
2016-10-27 23:52:23 +0400

    Initial commit

---
+++ Bot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bot
+//
+//  Created by Bagrat Kirakosian on 10/27/16.
+//  Copyright © 2016 PicsArt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bot/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bot
+//
+//  Created by Bagrat Kirakosian on 10/27/16.
+//  Copyright © 2016 PicsArt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
